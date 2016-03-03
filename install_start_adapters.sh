99bb665ba5634b0f90e0fa0a251e94d80bd5af59
ana-silva <anacristina.francodasilva@gmail.com>
2016-03-03 03:32:20 +0100

    Update install_start_adapters.sh

--- install_start_adapters.sh
+++ install_start_adapters.sh
@@ -1,28 +1,29 @@
 #!/bin/sh
-
+#
 #gets server IP
 #SERVER_IP="$(ifconfig | grep -v 'eth0:' | grep -A 1 'eth0' | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f 1)"
 #SERVER_IP="$(hostname -I)"
-
+#
 #passed through args
 RASPI_IP=$1
 SERVER_IP=$2
 #gets pins
 PIN_NR1=$3 #Ptrig
 PIN_NR2=$4 #Pecho
-
+#
 #copy script (todo: using ssh key)
 #scp ./sensoradapter_distance.py pi@$RASPI_IP:~/Desktop/sensoradapter_distance.py;
-
+#
 #ssh connection
 ssh pi@$RASPI_IP;
-
+#
 #install Mqtt client Paho
 pip install paho-mqtt;
-
+#
 #download script
 wget https://raw.githubusercontent.com/ana-silva/raspi-iot-prototypes/master/sensoradapter_distance.py;
-
+sleep 5;
+#
 #start script
-sudo python ~/sensoradapter_distance.py $SERVER_IP $PIN_NR1 $PIN_NR2 &
+sudo python ./sensoradapter_distance.py $SERVER_IP $PIN_NR1 $PIN_NR2;
 #this adapter pushes info to topic iot2/evt/distance/fmt/json
