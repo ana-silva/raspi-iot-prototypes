249d0cbb5bf02ec8c4efc9f052a470efe71abbe0
ana-silva <anacristina.francodasilva@gmail.com>
2016-03-03 13:09:58 +0100

    Update install_broker.sh

--- install_broker.sh
+++ install_broker.sh
@@ -3,11 +3,11 @@
 #
 #sudo apt-get install python # install python if necessary
 #
-sudo apt-get install python-software-properties
+sudo apt-get install python-software-properties;
 #sudo apt-get install python3-software-properties
-sudo apt-add-repository ppa:mosquitto-dev/mosquitto-ppa
-sudo apt-get update
-sudo apt-get install mosquitto mosquitto-clients python-mosquitto
+sudo apt-add-repository ppa:mosquitto-dev/mosquitto-ppa;
+sudo apt-get update;
+sudo apt-get install mosquitto mosquitto-clients python-mosquitto;
 #
 #starting mosquitto
-sudo mosquitto
+sudo mosquitto;
